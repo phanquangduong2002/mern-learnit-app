36035a4c4c9f227a6de2ea30ef0f4d6f90862fe2
phanquangduong2002 <phanquangduong2002@gmail.com>
2022-11-13 20:53:06 +0700

    Fix

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,9 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "http://localhost:5000/api"
-    : "https://sleepy-inlet-56101.herokuapp.com/api";
+    ? "https://mern-learn-it-app.herokuapp.com/api"
+    : "https://mern-learn-it-app.herokuapp.com/api";
+
+//http://localhost:5000/api
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 
